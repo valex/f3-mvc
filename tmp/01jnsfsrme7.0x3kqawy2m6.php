70b70b0be7c532ef15626c35e7f72c687447bfef
alexorg <alexorg@ukr.net>
2014-03-30 00:50:44 +0200

    rm cached files

--- tmp/01jnsfsrme7.0x3kqawy2m6.php
+++
@@ -1,3 +0,0 @@
-<?php echo $this->render('header.html',$this->mime,get_defined_vars()); ?>
-<?php echo $this->render($view,$this->mime,get_defined_vars()); ?>
-<?php echo $this->render('footer.html',$this->mime,get_defined_vars()); ?>
--- tmp/01jnsfsrme7.2lxxnmlklhu.php
+++
@@ -1,5 +0,0 @@
-    </div><!-- /.container -->
-    <script src="http://code.jquery.com/jquery.js"></script>
-    <script src="../../ui/js/bootstrap.min.js"></script>
-</body>
-</html>
